8db72310c7a90768a02e3b1a303bda2ee484dc78
LAWLK011 <kellylaw555@gmail.com>
2016-10-11 22:26:59 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book();
+            book.Title = "Intro to C#";
+            book.ISBN = 123567;
+            book.Price = 12.34M;
+            book.Count = 5;
+            book.PublishedYear = new DateTime(2010, 1, 1);
+
+            Library.AddBook(book);
+
+            book = new Book();
+            book.Title = "Intro to Python";
+            book.ISBN = 1758585858;
+            book.Price = 22.50M;
+            book.Count = 2;
+            book.PublishedYear = new DateTime(2012, 2, 1);
+
+            Library.AddBook(book);
+
+            Library.PrintBooks();
+        }
+    }
+}
