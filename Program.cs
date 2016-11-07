7987434deed82b41356ec392f6e518d452c55f9f
LAWLK011 <kellylaw555@gmail.com>
2016-11-06 17:31:06 -0800

    Build a Menu Class Oct 29 work

--- Program.cs
+++ Program.cs
@@ -10,25 +10,53 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-        //    var book = new Book();
-        //    book.Title = "Intro to C#";
-        //    book.ISBN = 123567;
-        //    book.Price = 12.34M;
-        //    book.Count = 5;
-        //    book.PublishedYear = new DateTime(2010, 1, 1);
+            Console.WriteLine("******Welcome to my library*******");
+            int choice = -1;
+            bool invalidChoice = false;
 
-        //    Library.AddBook(book);
+            while (choice != 0 || invalidChoice )
+            { 
+            Console.WriteLine("1. Add a book to the library");
+            Console.WriteLine("2. Rent a book from the library");
+            Console.WriteLine("3. Return a book to the library");
+            Console.WriteLine("0. Exit");
+            Console.Write("Please choose an option: ");
+            var input = Console.ReadLine();
+            if (!int.TryParse(input, out choice))
+            {
+                    invalidChoice = true;
+                    Console.WriteLine("Invalid choice. Try again...");
+                    continue;
+            }
 
-        //    book = new Book();
-        //    book.Title = "Intro to Python";
-        //    book.ISBN = 1758585858;
-        //    book.Price = 22.50M;
-        //    book.Count = 2;
-        //    book.PublishedYear = new DateTime(2012, 2, 1);
+            invalidChoice = false;
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Title: ");
+                        var title = Console.ReadLine();
+                        Console.Write("Price: ");
+                        decimal price;
+                        if (!decimal.TryParse(Console.ReadLine(), out price))
+                            return;
 
-        //    Library.AddBook(book);
+                        var book = new Book();
+                        book.Title = title;
+                        book.Price = price;
+                        book.PublishedYear = DateTime.Now;
+                        book.AuthorID = 1;
 
-           Library.PrintBooks();
+                        Library.AddBook(book);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        break;
+                }
+            }
+             Library.PrintBooks();
         }
     }
 }
